2a8a192b81478a7c408103db83316822de3d3096
ackeequokka <284396146+ackeequokka@users.noreply.github.com>
2026-09-17 20:33:25 +0200

    Create getComputerChoice func with random result

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice(rock, paper, scissors) {
+  rock = Math.random();
+  paper = Math.random();
+  scissors = Math.random();
+
+  if (rock > paper && rock > scissors) {
+    result = "rock";
+  } else if (paper > rock && paper > scissors) {
+    result = "paper";
+  } else {
+    result = "scissors";
+  }
+  return result;
+}
+
+console.log(getComputerChoice());
