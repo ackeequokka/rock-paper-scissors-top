c96a9c7f4168a36d709ed91418e1a3893785a684
ackeequokka <284396146+ackeequokka@users.noreply.github.com>
2026-09-18 19:00:36 +0200

    Create changeScore and playRound functions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let humanScore = 0;
 let computerScore = 0;
 
+// Returns randomized computer's choice as a number
+// Scissors = 1, paper = 2, rock = 3
 function getComputerChoice() {
   let rock = Math.random();
   let paper = Math.random();
@@ -15,31 +17,64 @@ function getComputerChoice() {
   }
 }
 
+// Gets human choice via prompt
 function getHumanChoice() {
   return prompt(
     "Please enter one of the following values: paper, rock or scissors",
   );
 }
 
+// Compares computer's and player's values once both are converted to numbers, defines the winner, returns the result
 function compareChoice(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
-    roundResult = "tie";
+    return "tie";
   } else {
     if (humanChoice < 3) {
       if (computerChoice - humanChoice == 1) {
-        roundResult = "win";
+        return "win";
       } else {
-        roundResult = "loose";
+        return "lose";
       }
     } else {
       if (humanChoice - compareChoice == 2) {
-        roundResult = "win";
+        return "win";
       } else {
-        roundResult = "loose";
+        return "lose";
       }
     }
   }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound() {
+  let computerChoice = getComputerChoice();
+  let humanChoice = convertHumanChoice();
+  roundResult = compareChoice(humanChoice, computerChoice);
+  changeScore(computerChoice, humanChoice);
+  return roundResult;
+}
+
+function changeScore(computerChoice, humanChoice) {
+  if (roundResult == "win") {
+    humanScore = humanScore + 1;
+    console.log("You won! Your score is " + humanScore);
+  } else if (roundResult == "lose") {
+    computerScore = computerScore + 1;
+    console.log("You won! Your score is " + computerScore);
+  } else {
+    console.log("It's a tie. Score was not changed");
+  }
+}
+
+// Converts human choice from prompt to a number, which is passed to comparison function
+function convertHumanChoice() {
+  let humanChoice = getHumanChoice();
+  if (humanChoice == "rock") {
+    return 3;
+  } else if (humanChoice == "paper") {
+    return 2;
+  } else {
+    return 1;
+  }
+}
+
+playRound();
